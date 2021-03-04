09cae85fefe545cdf651f7950c4ff1c623761c45
181268 <lujieni1017>
2021-03-04 11:23:02 +0800

    123

--- src/main/java/com/lujieni/resttemplate/controller/UseRestTemplate.java
+++ src/main/java/com/lujieni/resttemplate/controller/UseRestTemplate.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 /**
